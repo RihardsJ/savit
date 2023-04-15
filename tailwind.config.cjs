e95025e51151e8e6394afe88f2aa2b5936df16a5
RihardsJ <rihards.jukna@gmail.com>
2023-04-15 22:26:19 +0100

    adds bg image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    backgroundImage: {
+      savit: "url('src/images/savit-bg-image-1.png')",
+    },
     fontFamily: {
       raleway: ['Raleway', 'sans-serif'],
     },
