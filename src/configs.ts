678dbb98a5184ee4e0a883c5a0f2b31f7e9cbe52
RihardsJ <rihards.jukna@gmail.com>
2023-05-26 19:18:35 +0100

    adds responsiveness to a form

--- src/configs.ts
+++ src/configs.ts
@@ -2,12 +2,12 @@ const FIELDS = {
   CURRENT_ADDRESS: {
     LABEL: 'Current Address:',
     SLUG: 'current-address',
-    PLACEHOLDER: 'Start typing the address or postcode...',
+    PLACEHOLDER: 'Type current address or postcode...',
   },
   NEW_ADDRESS: {
     LABEL: 'New address:',
     SLUG: 'new-address',
-    PLACEHOLDER: 'Start typing the address or postcode...',
+    PLACEHOLDER: 'Type new address or postcode...',
   },
   DATE: { LABEL: 'Estimated moving date:', SLUG: 'date' },
   SIZE: { LABEL: 'Moving size:', SLUG: 'bedrooms' },
@@ -29,6 +29,8 @@ const STATUS = {
 
 const STYLE = {
   CLASSNAME: {
+    GLOBAL_STYLING:
+      'font-raleway bg-savit bg-bottom bg-no-repeat bg-cover h-screen text-grey px-12 pt-14 max-sm:px-6',
     LINE: 'border-solid border-2 transition-transform duration-300 ease-in-out w-full',
     BURGER_MENU_LINK: 'text-3xl mx-2.5 hover:underline',
     MAIN_TAG: 'max-h-[80vh] overflow-y-auto',
@@ -38,16 +40,18 @@ const STYLE = {
     HOME_BUTTON:
       'flex justify-center text-white text-2xl bg-brown rounded-md w-40 p-2 my-4',
     FORM_SUBMIT_MESSAGE:
-      'flex flex-col w-1/4 text-2xl bg-formBG/[0.6] p-4 rounded-sm',
+      'flex flex-col w-2/6 text-2xl bg-formBG/[0.6] p-4 rounded-sm',
     FORM_BUTTON:
       'bg-brown text-white my-2 p-3 w-3/4 text-2xl self-center rounded-sm',
-    FORM: 'flex flex-col w-1/4 bg-formBG/[0.6] p-4 rounded-sm',
+    FORM: 'flex flex-col lg:w-2/4 bg-formBG/[0.6] p-4 rounded-sm',
     LABEL: 'text-xl my-1 font-bold',
     SELECT: 'bg-transparent border border-solid border-grey  p-2',
-    TEXT: 'text-2xl my-6',
+    SKY_OVERLAY:
+      'fixed w-full h-full bg-sky opacity-60 top-0 left-0 z-[-1] sm:hidden',
+    TEXT: 'text-2xl my-6 max-sm:text-xl max-sm:text-center',
     TEXT_INPUT:
       'bg-transparent placeholder-grey border-solid border border-grey p-1',
-    TITLE: 'my-2 text-5xl font-semibold',
+    TITLE: 'my-2 text-5xl font-semibold max-sm:text-2xl max-sm:text-center',
     FLEX: {
       COLUMN: 'flex flex-col',
       ROW: 'flex flex-row',
